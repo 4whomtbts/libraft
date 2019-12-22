a274dc695e3be10a2862f892fa0fde09edd757f8
jun <4whomtbts@gmail.com>
2019-12-23 00:45:47 +0900

    init

---
+++ server/StudentServer.cc
@@ -0,0 +1,2 @@
+#include <grpcpp/grpcpp.h>
+#include "../proto-gen/Student.pb.h"
---
+++ server/StudentServer.h
@@ -0,0 +1,14 @@
+//
+// Created by jun on 19. 12. 22..
+//
+
+#ifndef LIBRAFT_STUDENTSERVER_H
+#define LIBRAFT_STUDENTSERVER_H
+
+
+class StudentServer {
+
+};
+
+
+#endif //LIBRAFT_STUDENTSERVER_H
